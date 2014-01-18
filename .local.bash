430084b23eba8914b413a991921ea4c97de1b564
simcap <caplette@hotmail.co.uk>
2014-01-18 22:39:39 +0100

    update bash prompt

--- .local.bash
+++ .local.bash
@@ -1,11 +1,10 @@
-
 function ruby_version() {
   version=$(ruby -v 2>/dev/null | cut -d' ' -f2)
   echo $version
 }
 
-source ~/.git-prompt.sh
-
-export PS1="\[\e[0;31m\]\w \[\e[0;33m\]\$(__git_ps1) \[\e[0;34m\]\$(ruby_version)\[\e[0m\]\n\$ "
+#export PS1="\[\e[0;31m\]\w \[\e[0;33m\]\$(__git_ps1) \[\e[0;34m\]\$(ruby_version)\[\e[0m\]\n\$ "
 
 chruby ruby-2.1.0
+
+source ~/.git-bash-prompt.sh
